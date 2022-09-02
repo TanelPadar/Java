1ff68c2e486100c6deda017c2a1c72e56854255e
padar <padar144@hotmail.com>
2022-09-02 15:35:52 +0300

    basic ex5

--- week1/Ex5.java
+++ week1/Ex5.java
@@ -3,6 +3,7 @@ package week1;
 public class Ex5 {
     public static void main(String[] args) {
         int days = 365;
+        int hours = 24;
         int minutes = 60;
         int seconds = 60;
         int secondsInYear = days * hours * minutes * seconds;
