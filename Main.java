0c2260a399528be4cf79fb579bb02c0d08bdaeab
padar <padar144@hotmail.com>
2022-09-02 15:23:08 +0300

    esimene töö javas

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // program code
+        String name =
     }
 }
