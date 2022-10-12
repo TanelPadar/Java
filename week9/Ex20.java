a38be74c159fd8bb577574acb370fa641be7db7d
TanelPadar <Padar144@hotmail.com>
2022-10-12 20:43:41 +0300

    Week 9

---
+++ week9/Ex20.java
@@ -0,0 +1,26 @@
+package week9;
+
+import week9.measurements.AverageSensor;
+import week9.measurements.ConstantSensor;
+import week9.measurements.Sensor;
+import week9.measurements.Thermometer;
+
+public class Ex20 {
+    public static void main(String[] args) {
+        Sensor kumpula = new Thermometer();
+        Sensor kaisaniemi = new Thermometer();
+        Sensor helsinkiVantaa = new Thermometer();
+
+        AverageSensor helsinkiArea = new AverageSensor();
+        helsinkiArea.addSensor(kumpula);
+        helsinkiArea.addSensor(kaisaniemi);
+        helsinkiArea.addSensor(helsinkiVantaa);
+
+        helsinkiArea.on();
+        System.out.println("the temperature in Helsinki area is "+helsinkiArea.measure() + " degrees");
+        System.out.println("the temperature in Helsinki area is "+helsinkiArea.measure() + " degrees");
+        System.out.println("the temperature in Helsinki area is "+helsinkiArea.measure() + " degrees");
+
+        System.out.println("readings: "+helsinkiArea.readings());
+    }
+}
