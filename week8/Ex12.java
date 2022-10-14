e8c157d2c3a9784f2b558c1ea56c97ad59996408
TanelPadar <Padar144@hotmail.com>
2022-10-14 12:56:23 +0300

    Coffee

--- week8/Ex12.java
+++ week8/Ex12.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ex12 {
     public static void main(String[] args) {
         Storehouse store = new Storehouse();
-        store.addProduct("coffee", 5, 10);
+        store.addProduct("Coffee", 5, 10);
         store.addProduct("milk", 3, 20);
         store.addProduct("milkbutter", 2, 55);
         store.addProduct("bread", 7, 8);
